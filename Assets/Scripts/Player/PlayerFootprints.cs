182f2196156a3b93c344006323f568dc03b34ab8
Kiliken <francescopaolomariani7@gmail.com>
2024-07-15 19:07:03 +0900

    footprints

---
+++ Assets/Scripts/Player/PlayerFootprints.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootprints : MonoBehaviour
+{
+    public bool isNegative = false;
+    public Transform reflector;
+
+    public Transform playerSprite;
+    Vector3 posReflect = Vector3.zero;
+    float side = 1f;
+    float distance;
+    void Start()
+    {
+        if (isNegative)
+            side = -1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(playerSprite.position, reflector.position);
+        transform.position = reflector.position + (new Vector3(distance * side,playerSprite.position.y,playerSprite.position.z));
+        transform.rotation = new Quaternion(playerSprite.rotation.x , playerSprite.rotation.y * -1f, playerSprite.rotation.z, playerSprite.rotation.w);
+    }
+}
