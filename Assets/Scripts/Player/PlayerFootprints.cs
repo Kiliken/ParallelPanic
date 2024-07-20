a2c1f0cddad7216675b65a7484d79f0180ab9866
Kiliken <francescopaolomariani7@gmail.com>
2024-07-20 18:15:54 +0900

    PlayerFootpritsSmallFix

--- Assets/Scripts/Player/PlayerFootprints.cs
+++ Assets/Scripts/Player/PlayerFootprints.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class PlayerFootprints : MonoBehaviour
 {
-    public bool isNegative = false;
     public Transform reflector;
 
     public Transform playerSprite;
     Vector3 posReflect = Vector3.zero;
-    float side = 1f;
     float distance;
-    void Start()
-    {
-        if (isNegative)
-            side = -1f;
-    }
 
     // Update is called once per frame
     void Update()
     {
         distance = playerSprite.position.x - reflector.position.x;
-        transform.position = reflector.position + (new Vector3(distance * side,playerSprite.position.y,playerSprite.position.z));
+        transform.position = reflector.position + (new Vector3(distance * -1f,playerSprite.position.y,playerSprite.position.z));
         transform.rotation = new Quaternion(playerSprite.rotation.x , playerSprite.rotation.y * -1f, playerSprite.rotation.z, playerSprite.rotation.w);
     }
 }
