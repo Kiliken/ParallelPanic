1c242d09a972bea01221a98843f5b03def4de6b8
Zayar Aung <z.zayar99@gmail.com>
2024-08-17 17:43:42 +0900

    heartbeat monitor

---
+++ Assets/Scripts/UI/Heartbeat.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heartbeat : MonoBehaviour
+{
+    Animator animator;
+    AudioSource audioSource;
+    public EnemyController enemy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemy.canMove){
+            animator.SetFloat("EnemyDist", enemy.playerDistance);
+        }
+        
+    }
+
+    public void PlaySFX(){
+        audioSource.Play();
+    }
+}
